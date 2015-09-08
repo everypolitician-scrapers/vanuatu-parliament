d0ddfd4504675cd0416f31af82f5523db93448e4
Tony Bowden <tony@tmtm.com>
2015-09-08 19:03:02 +0100

    initial scraper

---
+++ scraper.rb
@@ -0,0 +1,57 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  noko.css('.uk-table').xpath('.//tr[td]').each do |tr|
+    tds = tr.css('td')
+    name = tds[0].text.sub(/Hon.?\s*/, '').tidy
+
+    next if name.empty? || name.match(/Deces?ased/)
+
+    data = { 
+      name: name,
+      # role: tds[1].text.tidy,
+      party: tds[2].text.tidy,
+      constituency: tds[3].text.tidy,
+      term: 10,
+      source: url.to_s,
+    }
+
+    unless (mplink = tds[0].css('a/@href')).empty?
+      data.merge!(scrape_person(URI.join url, mplink.text))
+    end
+    puts data
+    # ScraperWiki.save_sqlite([:id, :term], data)
+  end
+end
+
+def scrape_person(url)
+  noko = noko_for(url)
+  data = { 
+    image: noko.css('a.thumbnail img/@src').text,
+    source: url.to_s,
+    id: url.to_s.split('/').last.split('-').first,
+  }
+  data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
+  data
+end
+
+scrape_list('http://parliament.gov.vu/index.php/2015-01-20-00-02-35')
