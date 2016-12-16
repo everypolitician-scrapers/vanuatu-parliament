95b1911fa8c25f06de297d0ca9ccb89e3ee7c2f5
Tony Bowden <tony@tmtm.com>
2016-12-16 11:23:32 +0000

    remove needless require of colorize

--- scraper.rb
+++ scraper.rb
@@ -3,10 +3,10 @@
 
 require 'scraperwiki'
 require 'nokogiri'
-require 'colorize'
 require 'pry'
-# require 'open-uri/cached'
-# OpenURI::Cache.cache_path = '.cache'
+
+# require 'open-uri/cached'
+# OpenURI::Cache.cache_path = '.cache'
 require 'scraped_page_archive/open-uri'
 
 class String
